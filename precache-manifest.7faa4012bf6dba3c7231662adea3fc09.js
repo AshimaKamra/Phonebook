a6b062917ea9d5ab77b5d6f978e9a3da893d4ce2
AshimaKamra <akkamra18@gmail.com>
2020-05-31 17:19:21 +0530

    Updates

---
+++ precache-manifest.7faa4012bf6dba3c7231662adea3fc09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebb42810a5641d0157f5933e5915790",
+    "url": "/AshimaKamra/Phonebook/index.html"
+  },
+  {
+    "revision": "83e7fb0b8ca08d891453",
+    "url": "/AshimaKamra/Phonebook/static/css/main.d463fe9b.chunk.css"
+  },
+  {
+    "revision": "a64692c101e826645d2a",
+    "url": "/AshimaKamra/Phonebook/static/js/2.871bd5d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AshimaKamra/Phonebook/static/js/2.871bd5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e7fb0b8ca08d891453",
+    "url": "/AshimaKamra/Phonebook/static/js/main.41116173.chunk.js"
+  },
+  {
+    "revision": "07243c2bbbf093f650ca",
+    "url": "/AshimaKamra/Phonebook/static/js/runtime-main.090fb9d7.js"
+  },
+  {
+    "revision": "6a05de4c1332b74a3214246466432857",
+    "url": "/AshimaKamra/Phonebook/static/media/logo.6a05de4c.jpg"
+  }
+]);
